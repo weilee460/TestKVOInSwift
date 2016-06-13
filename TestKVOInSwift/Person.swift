65f1913aed3ed14e7cbf5ca02d4458640ff5c997
Eagle <wei.lee460@gmail.com>
2016-06-13 09:16:53 +0800

    添加工程文件。  2016-06-13

---
+++ TestKVOInSwift/Person.swift
@@ -0,0 +1,49 @@
+//
+//  Person.swift
+//  TestKVOInSwift
+//
+//  Created by ying on 16/6/12.
+//  Copyright © 2016年 ying. All rights reserved.
+//
+
+import Foundation
+
+class Person: NSObject {
+    //dynamic表明支持 OC Runtime 动态分发机制
+    dynamic var firstName: String
+    dynamic var lastName: String
+    
+    var fullName: String {
+        get {
+            return "\(lastName) \(firstName)"
+        }
+    }
+    
+    init(firstName:String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    //实现 keyPathsForValuesAffectingValueForKey 方法在实体类中声明属性依赖
+    override class func keyPathsForValuesAffectingValueForKey(key: String) -> Set<String>
+    {
+        if key == "fullName" {
+            return Set<String>(arrayLiteral: "firstName","lastName")
+        } else {
+            return super.keyPathsForValuesAffectingValueForKey(key)
+        }
+    }
+    
+    //KVO 在默认情况下，只要为某个属性添加了监听对象，在这个属性值改变的时候，就会自动的通知监听者。也有一些情况下，可能我们想手动的处理这些通知的发送
+    //覆盖 automaticallyNotifiesObserversForKey 方法来告诉 KVO，那些属性是我们想手动处理的
+    override class func automaticallyNotifiesObserversForKey(key: String) -> Bool
+    {
+        if key == "firstName" {
+            //取消 firstName 这个属性KVO的自动通知
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+}
---
+++ TestKVOInSwift/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  TestKVOInSwift
+//
+//  Created by ying on 16/6/8.
+//  Copyright © 2016年 ying. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelFirstName: UILabel!
+    @IBOutlet weak var labelLastName: UILabel!
+    @IBOutlet weak var labelFullName: UILabel!
+    
+    private var person: Person?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.person = Person(firstName: "Peter", lastName: "Cook")
+        
+        //NSKeyValueObservingOptions.New， 这个选项代表我们监听 KVO 每次属性改变后的新值
+        self.person?.addObserver(self, forKeyPath: "firstName", options: NSKeyValueObservingOptions.New, context: nil)
+        self.person?.addObserver(self, forKeyPath: "lastName", options: NSKeyValueObservingOptions.New, context: nil)
+         self.person?.addObserver(self, forKeyPath: "fullName", options: NSKeyValueObservingOptions.New, context: nil)
+        //NSKeyValueObservingOptions.New 每次属性改变后的新值
+        //NSKeyValueObservingOptions.Old 每次属性改变之前的旧值
+        
+        //
+        labelFirstName.text = person?.firstName
+        labelLastName.text = person?.lastName
+        labelFullName.text = person?.fullName
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        
+        if keyPath == "firstName" {
+            if let firstName = change?[NSKeyValueChangeNewKey] as? String {
+                self.labelFirstName?.text = firstName
+            }
+        }
+        else if keyPath == "lastName" {
+            if let lastName = change?[NSKeyValueChangeNewKey] as? String {
+                self.labelLastName?.text = lastName
+            }
+        }
+        else if keyPath == "fullName" {
+            if let fullName = change?[NSKeyValueChangeNewKey] as? String {
+                self.labelFullName?.text = fullName
+            }
+        }
+        else {
+            
+        }
+    }
+    @IBAction func changeLabel(sender: UIButton) {
+        
+        self.person?.firstName = "Zi"
+        self.person?.lastName = "Feng"
+    }
+    
+    deinit {
+        super.viewDidLoad()
+        //将 KVO 通知的删除掉(如果没有正确的清除掉 KVO 通知，程序可能会在某些时候意外的崩溃)
+        self.person?.removeObserver(self, forKeyPath: "firstName")
+        self.person?.removeObserver(self, forKeyPath: "lastName")
+        self.person?.removeObserver(self, forKeyPath: "fullName")
+    }
+
+}
+
